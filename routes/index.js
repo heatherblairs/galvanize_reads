3400dcbda2f9c0d40830f7f792975da68046dcbe
Heather Stone <heather.blair.stone@gmail.com>
2016-07-12 14:42:13 -0600

    Add Book functionality

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,17 @@ router.get('/', function(req, res, next) {
   })
 });
 
+router.get('/add', function(req, res, next) {
+  res.render('add')
+});
+
+router.post('/add', function(req, res, next) {
+  knex('book').insert(req.body).then(function(){
+    res.redirect('/');
+  }).catch(function(err){
+    console.log(err)
+    next(err)
+  })
+});
+
 module.exports = router;
